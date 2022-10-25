64001fe7490502982e8af8e1a1573ac7f3bca55b
EthanHeuer <heuere9166@my.uwstout.edu>
2022-10-25 16:02:24 -0500

    Simple Data Input
    Devices now only need an asset tag. The model and purpose are pulled from the asset.

--- docs/js/device/device-dom.js
+++ docs/js/device/device-dom.js
@@ -4,32 +4,22 @@
 class DeviceDOM {
 	parent;
 	asset;
-	model;
-	type;
+	tag;
 	remove;
 
 	constructor (id) {
-		let item = DOM(null, "div", {classList: "flex-box horizontal pad device center"});
+		let item = DOM(null, "div", {classList: "flex-box horizontal pad gap device center"});
 		let name = DOM(item, "input", {classList: "device-asset",
 			dataset: {action: AppAction.DEVICE_ASSET, id: id}});
 
-		let model = DOM(item, "select", {classList: "device-model",
-			dataset: {action: AppAction.DEVICE_MODEL, id: id}});
-
-		for (let opt of [DeviceModel.HP, DeviceModel.MAC]) { DOM(model, "option", {innerHTML: opt, value: opt}); }
-
-		let type = DOM(item, "select", {classList: "device-type",
-			dataset: {action: AppAction.DEVICE_TYPE, id: id}});
-
-		for (let opt of [DeviceType.STUDENT, DeviceType.LOANER, DeviceType.FACULTY]) { DOM(type, "option", {innerHTML: opt, value: opt}); }
+		let tag = DOM(item, "input", {classList: "device-tag"});
 
 		let remove = DOM(item, "button", {innerHTML: "close", classList: "material-symbols-outlined icon", title: "Remove Device",
 			dataset: {action: AppAction.DEVICE_REMOVE, id: id}});
 
 		this.parent = item;
 		this.asset = name;
-		this.model = model;
-		this.type = type;
+		this.tag = tag;
 		this.remove = remove;
 	}
 }
--- docs/js/device/device.js
+++ docs/js/device/device.js
@@ -17,38 +17,64 @@ class DeviceType {
 
 class Device extends ArrayMapItem {
 	asset;
-	model;
-	type;
+	year = "";
+	model = "";
+	type = "";
+	tag;
 	time;
 	dom;
 
-	constructor (asset, model, type, id) {
+	static REGEX = [
+		[/^(?<year>\d\d\d\d)(?<purpose>550)(?<id>\d\d)$/m, ["HP", "Loaner"]],
+		[/^(?<year>\d\d\d\d)(?<purpose>554)(?<id>\d\d)$/m, ["Mac", "Loaner"]],
+		[/^(?<year>\d\d\d\d)(?<purpose>11)(?<id>\d\d\d)$/m, ["HP", "Student"]],
+		[/^(?<year>\d\d\d\d)(?<purpose>20)(?<id>\d\d\d)$/m, ["Mac", "Student"]],
+		[/^C-(?<id>\d\d\d\d\d)$/m, ["", "Faculty"]]
+	];
+
+	constructor (asset, id) {
 		super(id);
 
 		this.asset = asset;
-		this.model = model;
-		this.type = type;
 		this.time = Date.now();
 		this.dom = new DeviceDOM(id);
 
+		this.readAsset();
 		this.updateDom();
+	}
+
+	readAsset() {
+		for (let r = 0; r < Device.REGEX.length; r ++) {
+			let res = Device.REGEX[r][0].exec(this.asset);
+
+			if (res !== null) {
+				this.model = Device.REGEX[r][1][0];
+				this.type = Device.REGEX[r][1][1];
+
+				if (this.type !== "Faculty") {
+					this.year = res.groups.year;
+				}
+
+				break;
+			}
+		}
 	}
 
 	updateDom() {
 		this.dom.asset.value = this.asset;
 
-		for (let option of this.dom.model.getElementsByTagName("option")) {
-			option.selected = (option.value === this.model);
-		}
+		let tags = [];
 
-		for (let option of this.dom.type.getElementsByTagName("option")) {
-			option.selected = (option.value === this.type);
-		}
+		[this.year, this.model, this.type].forEach(e => {
+			if (e !== "") {
+				tags.push(e);
+			}
+		});
+
+		this.dom.tag.value = tags.join(" ");
 	}
 
 	export() {
-		let year = (this.asset.length === 9 ? this.asset.split("").splice(0,4).join("") : "");
-
-		return [this.asset, year, this.model, this.type];
+		return [this.asset, this.year, this.model, this.type];
 	}
 }
